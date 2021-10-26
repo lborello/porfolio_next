210ab481be5b1c2f16a37d27c3b8bd28aeea49bb
lborello <luis_borello@hotmail.com>
2021-10-26 18:24:34 -0300

    correct image

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -29,7 +29,7 @@ export const projects = [
   {
     title: 'ElectraBlue Kernel',
     description: "ElectraBlue is a flash & forget custom kernel for supported Android devices. It was developed with the aim to provide a stable, fluid, & battery-efficient experience with customizations.",
-    image: '/images/eb.jpg',
+    image: 'images/eb.jpg',
     tags: ['Linux', 'C', 'Makefile', 'Bash'],
     source: 'https://github.com/lordarcadius/electrablue_mido',
     visit: 'https://forum.xda-developers.com/t/kernel-mido-oreo-pie-electrablue-kernel-21-0-july-06-redmi-note-4.3655651/',
@@ -38,7 +38,7 @@ export const projects = [
   {
     title: 'Lenovo SNAPit',
     description: "Lenovo SNAPit Camera was one of the finest OEM camera apps back in those days. It had tonnes of unique and amazing features. I ported it to work on almost all devices at that time.",
-    image: '/images/snapit.jpg',
+    image: 'images/snapit.jpg',
     tags: ['Java', 'Libs', 'Smali'],
     source: '#',
     visit: 'https://forum.xda-developers.com/t/app-port-6-0-lenovo-snapit-camera-5-8-53-for-all-devices.3608065/',
@@ -47,7 +47,7 @@ export const projects = [
   {
     title: 'CyanogenOS Apps',
     description: "CyanogenOS 12.1 had a set of exclusive apps like a new theme engine and a new Truecaller integrated dialer. I ported it to work with CyanogenMod 13 and CM 13 based ROMs.",
-    image: '/images/capps.jpg',
+    image: 'images/capps.jpg',
     tags: ['Java', 'Libs'],
     source: '#',
     visit: 'https://forum.xda-developers.com/t/c-apps-v2-unofficial-6-0-x-cyanogen-os-capps-v2-for-cm13-and-cm13-based-roms.3254865/',
@@ -56,7 +56,7 @@ export const projects = [
   {
     title: 'CyanogenOS Apps',
     description: "CyanogenOS 12.1 had a set of exclusive apps like a new theme engine and a new Truecaller integrated dialer. I ported it to work with CyanogenMod 13 and CM 13 based ROMs.",
-    image: '/images/capps.jpg',
+    image: 'images/capps.jpg',
     tags: ['Java', 'Libs'],
     source: '#',
     visit: 'https://forum.xda-developers.com/t/c-apps-v2-unofficial-6-0-x-cyanogen-os-capps-v2-for-cm13-and-cm13-based-roms.3254865/',
@@ -66,6 +66,7 @@ export const projects = [
 ];
 
 export const TimeLineData = [
+  { year: 1999, text: 'Started my journey as a new developer.', },
   { year: 2016, text: 'Started my journey as a new developer.', },
   { year: 2017, text: 'Worked intensively on ROMs & Kernels.', },
   { year: 2018, text: 'Learned Android app development.', },
